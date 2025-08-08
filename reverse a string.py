072fdf3d461015b38ccfef7574be7f2492f9f628
esinwesleycoding <esin_mary@yahoo.com>
2025-08-08 18:07:11 +0100

    Add files via upload

---
+++ reverse a string.py
@@ -0,0 +1,12 @@
+# input a wrod or sentence
+string = input("Please enter your own String : ")
+
+
+string2 =('')
+#loop for printing in reverse
+for i in string:
+    string2 = i + string2
+
+
+print("\nThe Original String = ", string)
+print(" The Reverse String = ", string2)

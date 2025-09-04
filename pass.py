7d20e6afcedd56095eae2a96cebef36d50292b80
esinwesleycoding <esin_mary@yahoo.com>
2025-09-04 18:38:54 +0100

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,17 @@
+for x in range(10): #iterate for lopp
+    if x % 20 == 0: #condition 1
+        print("twist")
+
+
+    elif x % 15 == 0: #condition 2
+        pass      #pass statement
+
+
+    elif x % 5 == 0: #condition 3
+        print("fizz")   #display result
+
+    elif x % 3 == 0: #condition 4
+        print("buzz")   #displey result
+
+    else:              #condition 5
+        print(x)       #displey result

ca25cc744a37ed5e25d71cae71cd258b2d478857
esinwesleycoding <esin_mary@yahoo.com>
2025-09-11 18:49:38 +0100

    Update rock, paper, scissors.py

--- rock, paper, scissors.py
+++ rock, paper, scissors.py
@@ -5,24 +5,22 @@ while True: #iterate loop
     possible_actions = ["rock", "paper", "scissors"]
     #using random function
     computer_action = random.choice(possible_actions)
-    print(f"\nYou chose {user_action}, computer chose
-          {computer_action}.\n") #display both outputs what is selected by you and computer
 
     #conditions to check who won the game
-      if user_action == computer_action:
-    print(f"Both players selected {user_action}, It's a tie!")   
-     elif user_action == "rock":
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}, It's a tie!")   
+    elif user_action == "rock":
        if computer_action == "scissors":
          print("Rock smashes scissors! You win!")
        else:
         print("Paper covers rock! You lose."),
     elif user_action == "paper":
-       if computer_action == "rock"
+        if computer_action == "rock":
            print("Paper covers rock! You win!")
         else:
             print("Scissors cuts paper! You lose.")
     elif user_action == "scissors":
-        if computer_action == "paper"
+        if computer_action == "paper":
            print("Scissors cuts paper! You win!")
         else:
             print("Rock smashes scissors! You lose.")

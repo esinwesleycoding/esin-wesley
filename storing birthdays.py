f545935bb7f79895ff2d311cee20877b8d74b9de
esinwesleycoding <esin_mary@yahoo.com>
2025-09-12 16:31:48 +0100

    Create storing birthdays.py

---
+++ storing birthdays.py
@@ -0,0 +1,14 @@
+# Storing birthday dates in variables
+birthday1 = "12 January 1990"
+birthday2 = "23 March 1985"
+birthday3 = "05 July 2000"
+birthday4 = "19 October 1995"
+birthday5 = "30 December 1988"
+
+# Displaying the stored birthdays
+print("Stored Birthday Dates:")
+print(f"1. {birthday1}")
+print(f"2. {birthday2}")
+print(f"3. {birthday3}")
+print(f"4. {birthday4}")
+print(f"5. {birthday5}")

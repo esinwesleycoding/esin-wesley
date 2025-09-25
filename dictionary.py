006d59ee3426c854e2a5354d7205e73483c62ce7
esinwesleycoding <esin_mary@yahoo.com>
2025-09-25 18:38:55 +0100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+my_dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+# get vs[] for retrieving elements
+my_dict = {'name': 'Jack', 'age': 26}
+
+# Output: Jack
+print(my_dict['name'])
+
+# update value
+my_dict['Age'] = 6
+
+# add item
+my_dict['Gender'] = "Female"
+
+print("Dictionary -", my_dict)

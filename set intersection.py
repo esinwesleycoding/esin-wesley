bd2e9145c63637860b820a5706d7dc1d2d590e9a
esinwesleycoding <esin_mary@yahoo.com>
2025-09-26 18:08:17 +0100

    Add files via upload

---
+++ set intersection.py
@@ -0,0 +1,16 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
+
+
+set1 = {"blue", "yellow"}
+set2 = {"green", "red", "yellow"}
+print("original elements")
+print(set1)
+print(set2)
+print("union of set1 and set2",set1.union(set2))
